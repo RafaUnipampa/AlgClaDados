62de30dcd997f6bf6036fd9c9427536348fb8890
RafaUnipampa <RafaUnipampa@users.noreply.github.com>
2025-09-01 02:43:28 -0300

    Testando

---
+++ src/main/java/Exercicios/Lampada.java
@@ -0,0 +1,15 @@
+package Exercicios;
+
+public class Lampada {
+    private boolean energizada;
+    public void energizar(){
+        energizada = true;
+
+    }
+    public void desenergizar(){
+        energizada = false;
+    }
+    public boolean mostrar(){
+        return energizada;
+    }
+}
---
+++ src/main/java/Exercicios/MainLampada.java
@@ -0,0 +1,14 @@
+package Exercicios;
+
+public class MainLampada {
+    public static void main(String[] args) {
+        Lampada lampada = new Lampada();
+
+        System.out.println("Estado Inicial: "+ lampada.mostrar());
+        lampada.energizar();
+        System.out.println("Depois de energizar "+ lampada.mostrar());
+        lampada.desenergizar();
+        System.out.println("Depois de tirar a energia: "+lampada.mostrar());
+    }
+
+        }
